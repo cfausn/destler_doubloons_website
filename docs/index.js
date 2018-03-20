2ef2383e852c9cddcaa062dd430ae3dba308b98f
Colin Fausnaught <cfausn@gmail.com>
2018-03-20 14:34:09 -0400

    pls fix

--- docs/index.js
+++ docs/index.js
@@ -13,7 +13,7 @@ window.addEventListener('load', function() {
   } else {
     console.log('No web3? You should consider trying MetaMask!')
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8080"));
+    web3 = new Web3(new Web3.providers.HttpProvider("http://destler-doubloons.com/"));
   }
 
   token = web3.eth.contract(abi).at(contractAddress)
